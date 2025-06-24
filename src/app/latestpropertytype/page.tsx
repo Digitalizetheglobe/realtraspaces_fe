a1a081edfd7feff3442d58b81736524c7eb965d9
Online-Educational sysytemgit config --global user.name Online-Educational <tiwalkarashwini@gmail.com>
2025-06-24 18:22:30 +0530

    latestproperty

--- src/app/latestpropertytype/page.tsx
+++ src/app/latestpropertytype/page.tsx
@@ -144,7 +144,7 @@ export default function PropertyCards() {
                       <h3 className="font-medium text-black text-base">
                         {property.title || "Prime Business Hub"}
                       </h3>
-                      <div className="flex items-center text-gray-500 text-xs">
+                      <div className="flex items-center text-gray-700 text-xs">
                         <svg className="w-3 h-3 mr-1" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                           <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z" />
                           <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M15 11a3 3 0 11-6 0 3 3 0 016 0z" />
@@ -152,8 +152,8 @@ export default function PropertyCards() {
                         {property.address?.subLocality || property.address?.city || "Location Name"}
                       </div>
                     </div>
-                    <div className="w-5 h-5 border border-gray-400 rounded flex items-center justify-center">
-                      <svg className="w-3 h-3" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                    <div className="w-5 h-5 border border-gray-600 rounded flex items-center justify-center">
+                      <svg className="w-3 h-3 text-black" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                         <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M5 13l4 4L19 7" />
                       </svg>
                     </div>
