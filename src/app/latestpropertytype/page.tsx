bbc0dccff3577bbeab4113402d5ee50062990d3e
Online-Educational sysytemgit config --global user.name Online-Educational <tiwalkarashwini@gmail.com>
2025-06-24 18:24:59 +0530

    text color

--- src/app/latestpropertytype/page.tsx
+++ src/app/latestpropertytype/page.tsx
@@ -140,12 +140,12 @@ export default function PropertyCards() {
                 >
                   {/* Header with title and checkbox */}
                   <div className="p-3 flex justify-between items-center">
-                    <div>
+                    <div className="h-14">
                       <h3 className="font-medium text-black text-base">
                         {property.title || "Prime Business Hub"}
                       </h3>
                       <div className="flex items-center text-gray-700 text-xs">
-                        <svg className="w-3 h-3 mr-1" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                        <svg className="w-4 h-4 mr-1" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                           <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z" />
                           <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M15 11a3 3 0 11-6 0 3 3 0 016 0z" />
                         </svg>
